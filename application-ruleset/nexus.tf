b757e409ac51cc6ed9cc0992d0241d6f70dc16e9
Diogo Regateiro <diogo.regateiro@atlar.pt>
2022-10-31 16:11:24 +0000

    BANG-22: Add docker ports to the security group.

--- application-ruleset/nexus.tf
+++ application-ruleset/nexus.tf
@@ -13,6 +13,41 @@ locals {
       protocol  = "tcp"
       ports     = [8081]
       target    = "network"
+    },
+    nexus_docker_hosted_ingress = {
+      service   = "nexus_docker_hosted_ingress"
+      direction = "ingress"
+      protocol  = "tcp"
+      ports     = [9080]
+      target    = "network"
+    },
+    nexus_docker_proxy_ingress = {
+      service   = "nexus_docker_proxy_ingress"
+      direction = "ingress"
+      protocol  = "tcp"
+      ports     = [9081]
+      target    = "network"
+    },
+    nexus_docker_group_ingress = {
+      service   = "nexus_docker_group_ingress"
+      direction = "ingress"
+      protocol  = "tcp"
+      ports     = [9082]
+      target    = "network"
+    },
+    nexus_docker_quarantine_ingress = {
+      service   = "nexus_docker_quarantine_ingress"
+      direction = "ingress"
+      protocol  = "tcp"
+      ports     = [9083]
+      target    = "network"
+    },
+    nexus_central_hosted_ingress = {
+      service   = "nexus_central_hosted_ingress"
+      direction = "ingress"
+      protocol  = "tcp"
+      ports     = [9084]
+      target    = "network"
     }
   }
 }
