be3a1cbfd40b050845da8494b334174d7c8fc4b1
Pedro Osório <pedroosorio.eeic@gmail.com>
2022-11-07 18:14:56 +0000

    ST-1415: Added reverseproxy ruleset

---
+++ application-ruleset/reverseproxy.tf
@@ -0,0 +1,25 @@
+locals {
+  reverseproxy_rules = {
+    reverseproxy_ingress = {
+      service   = "reverseproxy"
+      direction = "ingress"
+      protocol  = "tcp"
+      ports     = [80, 443]
+      target    = "public"
+    }
+    reverseproxy_k8s_haproxy = {
+      service   = "reverseproxy"
+      direction = "ingress"
+      protocol  = "tcp"
+      ports     = [9443, 6443]
+      target    = "network"
+    }
+    reverseproxy_thanos_sidecar = {
+      service   = "reverseproxy"
+      direction = "ingress"
+      protocol  = "tcp"
+      ports     = [10901]
+      target    = "network"
+    }
+  }
+}
