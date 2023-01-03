03640e8db5346cbaa967200b11e9a8cefcd0fd19
Domingos Nunes <domingos.nunes@atlar.pt>
2023-01-03 15:16:56 +0000

    ST-1443: fix jump_host variable was not optional

--- openstack-instance/configurations.tf
+++ openstack-instance/configurations.tf
@@ -5,6 +5,6 @@ locals {
     availability_zone = coalesce(var.configuration.availability_zone, var.defaults.availability_zone)
     network           = coalesce(var.configuration.network, var.defaults.network)
     keypair           = coalesce(var.configuration.keypair, var.defaults.keypair)
-    jump_host         = coalesce(var.configuration.jump_host, var.defaults.jump_host)
+    jump_host         = try(coalesce(var.configuration.jump_host, var.defaults.jump_host), null)
   })
 }
