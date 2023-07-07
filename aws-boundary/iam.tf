21d51de079e741e735170806eddaae726a4bdb87
Pete Prior <pete.prior@skao.int>
2023-07-07 12:02:51 +0100

    SDR-866 Define EC2 instance role and IAM policy for KMS access

---
+++ aws-boundary/iam.tf
@@ -0,0 +1,54 @@
+resource "aws_iam_instance_profile" "controller" {
+  name = "controller_profile"
+  role = aws_iam_role.controller_role.name
+}
+
+data "aws_kms_key" "boundary_kms" {
+  key_id = aws_kms_key.kms_key.key_id
+}
+
+data "aws_iam_policy_document" "assume_policy" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+data "aws_iam_policy_document" "controller_kms_policy" {
+  statement {
+    effect = "Allow"
+
+    actions =  ["kms:Encrypt",
+                "kms:Decrypt",
+                "kms:ReEncrypt*",
+                "kms:GenerateDataKey*",
+                "kms:DescribeKey"]
+  
+    resources = ["${data.aws_kms_key.boundary_kms.arn}"]
+  }
+}
+
+resource "aws_iam_policy" "controller_kms_access" {
+  name        = "Controller_KMS_access"
+  description = "Allows controller instances access to KMS"
+  path        = "/"
+
+  policy = data.aws_iam_policy_document.controller_kms_policy.json
+}
+
+resource "aws_iam_role" "controller_role" {
+  name               = "controller_role"
+  path               = "/"
+  assume_role_policy = data.aws_iam_policy_document.assume_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "controller_kms_access" {
+  role       = aws_iam_role.controller_role.name
+  policy_arn = aws_iam_policy.controller_kms_access.arn
+}
